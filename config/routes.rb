1c1733afe59bf22485a4f4cf602f8dacff96716b
Yasir Malik <yasirrafiq786@gmail.com>
2020-04-25 14:36:03 +1000

    added editing feature

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   get "tasks/new", to: "tasks#new", as: :new
   get "tasks/:id", to: "tasks#show", as: :task
   post "tasks", to: "tasks#create"
+  get "tasks/edit/:id", to: "tasks#edit"
+  patch "tasks/:id", to: "tasks#update"
 end
